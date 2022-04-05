bdb139a284134f9f88391f0ba5b0c652226f31e2
Mohd Shahrukh <md.shahrukhmalik2012@gmail.com>
2022-04-06 00:04:14 +0530

    added functionalities of login page

--- quora-website/src/App.js
+++ quora-website/src/App.js
@@ -1,9 +1,20 @@
+import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { store } from './Redux/store';
+import { Home } from './Routes/HomePage';
 import { Login } from './Routes/LoginPage';
+import { NoRoute } from './Routes/NoRoutes';
 
 function App() {
+  const store = useSelector((state)=>state)
+  console.log(store)
   return (
     <div>
-      <Login />
+      <Routes>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/' element={<Home />} />
+        <Route path="*" element={<NoRoute />} />
+      </Routes>
     </div>
   );
 }
