89cd1acef2963e525aa69188aa62fa9034782419
a4aks <a4ashish04@gmail.com>
2022-04-07 22:16:48 +0530

    Post page updated

--- quora-website/src/HomePageComponents/Component1.jsx
+++ quora-website/src/HomePageComponents/Component1.jsx
@@ -6,8 +6,27 @@ import React, { useEffect } from "react";
 import { PostTop } from "./Postpage/PostTop";
 import { PostBottom } from "./Postpage/PostBottom";
 import { PostMiddle } from "./Postpage/PostMiddle";
+import { useDispatch, useSelector } from "react-redux";
+import { getPost } from "../Redux/PostReducer/action";
 
 export const Component1 = () => {
+
+  const {posts} = useSelector((state) => state.postReducer);
+  // console.log("post", posts);
+  const dispatch = useDispatch();
+  const getPosts = () => {
+    fetch(`http://localhost:3001/post`)
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log("res:", res);
+        dispatch(getPost(res));
+      });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <>
       <div
@@ -48,18 +67,33 @@ export const Component1 = () => {
           </Button>
         </div>
       </div>
-      <div
-        style={{
-          backgroundColor: "white",
-          border: "1px solid black",
-          margin: "20px",
-          padding: "20px",
-        }}
-      >
-        <PostTop />
-        <PostMiddle />
-        <PostBottom />
-      </div>
+            
+        {posts.map((post) =>(
+          <div
+          style={{
+            backgroundColor: "white",
+            border: "1px solid black",
+            margin: "20px",
+            padding: "20px",
+          }}
+        >
+         <div style={{ display: "flex" }}>
+          <Avatar />
+          <div>
+            <h3>Ashish Kumar</h3>
+            <span>
+              Masters in Economics, IIT KGP and Co Founder at Skillslash Academy.
+            </span>
+          </div>
+        </div>
+        <div>
+        <h4>{post.title}</h4>
+        <img src = {post.images} alt = "" style={{width:"500px"}}/>
+        </div>
+          <PostBottom />
+           </div>
+        ))}
+      
     </>
   );
 };
--- quora-website/src/HomePageComponents/Postpage/PostMiddle.jsx
+++ quora-website/src/HomePageComponents/Postpage/PostMiddle.jsx
@@ -4,26 +4,27 @@ import { getPost } from '../../Redux/PostReducer/action';
 import { ShowPost } from './ShowPost';
 
 
-export const PostMiddle = () => {
-    const {posts} = useSelector((state) => state.postReducer);
-    console.log("post", posts);
-    const dispatch = useDispatch();
-    const getPosts = () => {
-      fetch(`http://localhost:3001/post`)
-        .then((res) => res.json())
-        .then((res) => {
-          console.log("res:", res);
-          dispatch(getPost(res));
-        });
-    };
+export const PostMiddle = ({post}) => {
+  console.log("post2", post)
+    // const {posts} = useSelector((state) => state.postReducer);
+    // console.log("post", posts);
+    // const dispatch = useDispatch();
+    // const getPosts = () => {
+    //   fetch(`http://localhost:3001/post`)
+    //     .then((res) => res.json())
+    //     .then((res) => {
+    //       console.log("res:", res);
+    //       dispatch(getPost(res));
+    //     });
+    // };
   
-    useEffect(() => {
-      getPosts();
-    }, []);
+    // useEffect(() => {
+    //   getPosts();
+    // }, []);
   
   return (
     <div>
-      {posts.map((post) =>(
+      {post.map((post) =>(
            <h4>{post.title}</h4>
       ))}
     </div>
