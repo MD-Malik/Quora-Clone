35a80c3aea175004fe3bb5087d5dec8a4ec26672
Mohd Shahrukh <md.shahrukhmalik2012@gmail.com>
2022-05-02 13:45:44 +0530

    added backend

---
+++ Backend/Controller/user.js
@@ -0,0 +1,8 @@
+function getAllUser(req, res, next){
+    console.log("hello")
+    res.send('hellp')
+}
+
+module.exports = {
+    getAllUser
+}
---
+++ Backend/DBConnection/mongodb.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+class mongo{
+    constructor(){
+        this.mongodbconnection()
+    }
+
+    mongodbconnection(){
+        mongoose.connect('mongodb+srv://shahrukh:2012@cluster0.ndx7a.mongodb.net/QuoraDB?retryWrites=true&w=majority')
+        mongoose.connection.once('open', ()=>{
+            console.log('MongoDB is Connected');
+        })
+        mongoose.connection.on('error', ()=>{
+            consolelog("error in mongodb connection")
+        })
+    }
+}
+
+
+module.exports = mongo;
