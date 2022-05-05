f236886971f93a58b94f314e379cb9051f0e8007
Upendra-99 <chauhanup47@gmail.com>
2022-05-05 22:35:37 +0530

    google auth

--- Backend/Authentication/googleLogin.js
+++ Backend/Authentication/googleLogin.js
@@ -1,25 +1,27 @@
+const passportForGoogle = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
-const passport = require("passport")
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
-
-
-passport.serializeUser(function (user, done) {
-    done(null, user);
+passportForGoogle.serializeUser(function (user, done) {
+  done(null, user);
 });
 
-passport.deserializeUser(function (user, done) {
-    done(null, user);
+passportForGoogle.deserializeUser(function (user, done) {
+  done(null, user);
 });
 
-passport.use(new GoogleStrategy({
-    clientID: "281913688914-lsut6alanqb38gqoagkfc9lok484fs2e.apps.googleusercontent.com",
-    clientSecret: "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",
-    callbackURL: "http://localhost:9008/google/callback",
-    passReqToCallback: true
-},
+passportForGoogle.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "281913688914-lsut6alanqb38gqoagkfc9lok484fs2e.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "http://localhost:9008/google/callback",
+      passReqToCallback: true,
+    },
     function (request, accessToken, refreshToken, profile, done) {
-        return done(null, profile);
+      return done(null, profile);
     }
-));
+  )
+);
 
-module.exports = passport;
+module.exports = passportForGoogle;
--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -1,72 +1,69 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
-const userController = require('../Controller/user')
-const passportFacebook = require('../Authentication/facebookLogin');
-const passportGoogle = require('../Authentication/googleLogin');
-const user = require('../Models/user');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const userController = require("../Controller/user");
+const passportFacebook = require("../Authentication/facebookLogin");
+const googleOuth = require("../Authentication/googleLogin");
+const user = require("../Models/user");
+const cors = require("cors");
 
-app.use(bodyParser.json([]))
-app.use(passportGoogle.initialize());
-app.use(cors())
+app.use(bodyParser.json([]));
+app.use(googleOuth.initialize());
+app.use(cors());
 
+app.get("/getAllUsers", userController.getAllUsers);
 
+app.post("/register", userController.registerUser);
 
+app.post("/signIn", userController.signIn);
 
-app.get('/getAllUsers', userController.getAllUsers)
+app.delete("/logout/:token", userController.logout);
 
-app.post('/register', userController.registerUser);
+app.get("/", userController.loginUsingFacebookSuccess);
 
-app.post('/signIn', userController.signIn)
+app.get("/login", (req, res, next) => {
+  res.send("failed");
+});
 
-app.delete('/logout/:token', userController.logout)
+app.get("/auth/facebook", passportFacebook.authenticate("facebook"));
 
-app.get('/', userController.loginUsingFacebookSuccess)
+app.get(
+  "/auth/facebook/callback",
+  passportFacebook.authenticate("facebook", { failureRedirect: "/login" }),
+  userController.loginUsingFacebook
+);
 
-app.get('/login', (req, res, next)=>{
-    res.send("failed");
-})
+app.get("/failed", (req, res) => {
+  res.send("Some error occured while logging in to google");
+});
 
-app.get('/auth/facebook',
-  passportFacebook.authenticate('facebook'));
+app.get("/success", (req, res) => {
+  res.send("Successfully logged in to google");
+});
 
-app.get('/auth/facebook/callback',
-  passportFacebook.authenticate('facebook', { failureRedirect: '/login' }),
-  userController.loginUsingFacebook);
-
-
-
-  app.get('/failed', (req,res)=>{
-      res.send("Some error occured while logging in to google")
+app.get(
+  "/google",
+  googleOuth.authenticate("google", {
+    scope: ["email", "profile"],
   })
-  
-  app.get('/success', (req, res)=>{
-      res.send("Successfully logged in to google")
-  })
-  
-  app.get('/google', passportGoogle.authenticate('google',{
-      scope:
-      ['email', 'profile']
-  }
-  ));
-  
-  app.get('/google/callback',
-     passportGoogle.authenticate('google', {
-         failureRedirect: '/failed',
-     }),
-     userController.loginUsingGoogle
-  );
+);
 
-  app.post('/forgotPassword', userController.forgotPassword)
+app.get(
+  "/google/callback",
+  googleOuth.authenticate("google", {
+    failureRedirect: "/failed",
+  }),
+  userController.loginUsingGoogle
+);
 
-  app.post('/emailConfirmation', userController.emailConfirmation)
+app.post("/forgotPassword", userController.forgotPassword);
 
-  app.post('/resetPassword', userController.resetPassword)
+app.post("/emailConfirmation", userController.emailConfirmation);
 
-  app.get('/user/:token', userController.getUserByToken)
+app.post("/resetPassword", userController.resetPassword);
 
-  app.get('/verifyToken/:token', userController.verifyToken)
+app.get("/user/:token", userController.getUserByToken);
 
+app.get("/verifyToken/:token", userController.verifyToken);
 
 module.exports = app;
